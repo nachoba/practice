290dfcdbb11de94fcea353d63e8857655d6bc4d4
nachoba <0800nacho@gmail.com>
2017-07-31 18:00:44 -0300

    another kata

---
+++ codewars/SumOfNumbers.hs
@@ -0,0 +1,27 @@
+-- Sum of Numbers from 0 to N
+-- --------------------------
+--
+-- https://www.codewars.com/kata/sum-of-numbers-from-0-to-n/train/haskell
+--
+-- We want to generate a function that computes the series starting from 0 and
+-- ending until the given number following the sequence:
+-- 0,1,3,6,10,15...
+-- Which si created by:
+-- 0, 0+1, 0+1+2, 0+1+2+3, 0+1+2+3+4,...
+--
+-- seq 6   = 0+1+2+3+4+5+6 = 21
+-- seq -15 = -15<0
+-- seq   0 = 0
+
+sequenceSum :: Int -> String
+sequenceSum x | x < 0     = show x ++ " < 0"
+              | otherwise = readNumber x ++ show(sum[0..x])
+
+
+
+-- Read a number
+readNumber :: Int -> String
+readNumber 0 = "0 = 0"
+readNumber 1 = "1+0 = " 
+readNumber x = show x ++ "+" ++ readNumber (x -1)
+
